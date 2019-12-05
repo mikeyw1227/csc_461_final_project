005b3f3e8c9872fda537457aa5a694d4904792c0
Michael <mikeyw1227@my.uri.edu>
2019-12-05 09:06:00 -0500

    decision tree model started

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 
 test_size = 0.25
 batch_size = 1000
@@ -9,6 +10,11 @@ batch_size = 1000
 def get_data(csv_file):
     data_set = pd.read_csv(csv_file)
     return data_set.to_numpy()
+
+
+def decision_tree(x, y):
+    classifier = tree.DecisionTreeClassifier(max_depth=10)
+    return classifier.fit(x, y)
 
 
 def main():
