1767b8ee736473e307932587ea663fb49d0abd5f
Michael <mikeyw1227@my.uri.edu>
2019-12-06 13:43:34 -0500

    cleaned the data

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
-import statsmodels.api as sm
+from sklearn import preprocessing
 
 test_size = 0.25
 batch_size = 1000
 
 
 def get_data(csv_file):
-    data_set = pd.read_csv(csv_file)
-    return data_set.to_numpy()
+    return pd.read_csv(csv_file)
 
 
 def decision_tree(x, y):
@@ -19,16 +18,18 @@ def decision_tree(x, y):
 
 
 def main():
-    file_name = "adult.data"
+    file_name = "adult.csv"
     data = get_data(file_name)
-    training, validate = train_test_split(data, test_size=test_size)
-    # separate feature vectors from labels
-    x_train = training[:, :-1]
-    print(x_train)
-    
-    y_train = training[:, -1]
-    # convert labels to 0 or 1
-    _, labels = np.unique(y_train, return_inverse=True)
+    data = data.replace(' ?', np.nan)
+    data = data.dropna(axis=0)
+    y_data = data.iloc[:, -1]
+    x_data = data.drop(data.columns[-1], axis=1)
+    data[data.select_dtypes(['object']).columns] = data.select_dtypes(['object']).apply(lambda x: x.astype('category'))
+    cat_cols = data.select_dtypes(['category']).columns
+    data[cat_cols] = data[cat_cols].apply(lambda x: x.cat.codes)
+    print(data.head())
+    x_train, x_valid, y_train, y_valid = train_test_split(x_data, y_data, test_size=test_size)
+    # decision_tree(x_train, y_train)
 
 
 if __name__ == "__main__":
