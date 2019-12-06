1810f6445278f57fafcc0ac29c3edf6ed813639b
Michael <mikeyw1227@my.uri.edu>
2019-12-06 15:06:21 -0500

    added some more models

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
-from sklearn import preprocessing
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 
 test_size = 0.25
 batch_size = 1000
@@ -13,7 +16,17 @@ def get_data(csv_file):
 
 
 def decision_tree(x, y):
-    classifier = tree.DecisionTreeClassifier(max_depth=10)
+    classifier = tree.DecisionTreeClassifier(max_depth=15)
+    return classifier.fit(x, y)
+
+
+def svm(x, y):
+    classifier = SVC(gamma='auto')
+    return classifier.fit(x, y)
+
+
+def multilayer_perceptron(x, y):
+    classifier = MLPClassifier()
     return classifier.fit(x, y)
 
 
@@ -22,14 +35,21 @@ def main():
     data = get_data(file_name)
     data = data.replace(' ?', np.nan)
     data = data.dropna(axis=0)
-    y_data = data.iloc[:, -1]
-    x_data = data.drop(data.columns[-1], axis=1)
     data[data.select_dtypes(['object']).columns] = data.select_dtypes(['object']).apply(lambda x: x.astype('category'))
     cat_cols = data.select_dtypes(['category']).columns
     data[cat_cols] = data[cat_cols].apply(lambda x: x.cat.codes)
-    print(data.head())
+    y_data = data.iloc[:, -1]
+    x_data = data.drop(data.columns[-1], axis=1)
     x_train, x_valid, y_train, y_valid = train_test_split(x_data, y_data, test_size=test_size)
-    # decision_tree(x_train, y_train)
+    plt.figure()
+    # decision tree
+    # dt_classifier = decision_tree(x_train, y_train)
+    # plot_tree(dt_classifier, filled=True)
+    # plt.show()
+    # support vector machine
+    # svm_classifier = svm(x_data, y_data)
+    # multilayer perceptron
+    mlp_classifier = multilayer_perceptron(x_data, y_data)
 
 
 if __name__ == "__main__":
