4c7a7fe07043275456ec81eb84fcd3b25f56433b
Chris Paterno <cpaterno@my.uri.edu>
2019-12-08 13:00:05 -0500

    More models

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn import tree, preprocessing
-import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier, Perceptron
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from enum import Enum
 
@@ -45,7 +46,7 @@ def results(model, x_test, y_test):
 
 
 def decision_tree(x, y, params):
-    classifier = tree.DecisionTreeClassifier()
+    classifier = DecisionTreeClassifier()
     optimized_classifier = GridSearchCV(classifier, params)
     return optimized_classifier.fit(x, y)
 
@@ -94,6 +95,7 @@ def train_sgd_svm(x_train, x_valid, y_train, y_valid):
     results(sgdsvm_classifier, x_valid, y_valid)
     return
 
+
 def train_sgd_lg(x_train, x_valid, y_train, y_valid):
     # SGD Logistic Regression
     sgdlg_params = {
@@ -107,6 +109,74 @@ def train_sgd_lg(x_train, x_valid, y_train, y_valid):
     results(sgdlg_classifier, x_valid, y_valid)
     return
 
+def logistic_regression(x, y, params):
+    classifier = LogisticRegression()
+    optimized_classifier = GridSearchCV(classifier, params)
+    return optimized_classifier.fit(x, y)
+
+
+def train_lg(x_train, x_valid, y_train, y_valid):
+    # Logistic Regression
+    lg_params = {
+        'penalty': ('l1', 'l2', 'none', 'elasticnet'),
+        'solver': ('newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'),
+        'max_iter': [i for i in range(100, 600, 100)]
+    }
+    lg_classifier = logistic_regression(x_train, y_train, lg_params)
+    results(lg_classifier, x_valid, y_valid)
+    return
+
+
+def multilayer_perceptron(x, y, params):
+    classifier = MLPClassifier()
+    optimized_classifier = GridSearchCV(classifier, params)
+    return optimized_classifier.fit(x, y)
+
+
+def train_mlp(x_train, x_valid, y_train, y_valid):
+    # MLP
+    n = int(2 / 3 * x_train.shape[1])  # suitable because source
+    mlp_params = {
+        'hidden_layer_sizes': [],
+        'activation': ('logistic', 'identity', 'tanh', 'relu'),
+        'solver': ('lbfgs', 'sgd', 'adam'),
+        'learning_rate': ('constant', 'invscaling')
+    }
+    for i in range(2, 11):
+        mlp_params['hidden_layer_sizes'].append([n for _ in range(i)])
+    mlp_classifier = multilayer_perceptron(x_train, y_train, mlp_params)
+    results(mlp_classifier, x_valid, y_valid)
+    return
+
+def linear_perceptron(x, y, params):
+    classifier = Perceptron()
+    optimized_classifier = GridSearchCV(classifier, params)
+    return optimized_classifier.fit(x, y)
+
+
+def train_lp(x_train, x_valid, y_train, y_valid):
+    lp_params = {'penalty': ['l2', 'l1', 'elasticnet'],
+                 'max_iter': [1000, 5000, 10000]}
+    lp_classifier = linear_perceptron(x_train, y_train, lp_params)
+    results(lp_classifier, x_valid, y_valid)
+    return
+
+
+def random_forest(x, y, params):
+    classifier = RandomForestClassifier()
+    optimized_classifier = GridSearchCV(classifier, params)
+    return optimized_classifier.fit(x, y)
+
+
+def train_rf(x_train, x_valid, y_train, y_valid):
+    # random forest
+    rf_params = {'criterion': ['gini', 'entropy'],
+                 'n_estimators': [i for i in range(10, 101, 10)],
+                 'max_depth': [i for i in range(5, 105, 10)]}
+    rf_classifier = random_forest(x_train, y_train, rf_params)
+    results(rf_classifier, x_valid, y_valid)
+    return
+
 
 def main():
     # get and clean data
@@ -115,35 +185,41 @@ def main():
     x_data = data.drop(data.columns[-1], axis=1)
     test_size = 0.25
     x_train, x_valid, y_train, y_valid = train_test_split(x_data, y_data, test_size=test_size)
-    e = Model.DT
+    e = Model.PERCEP
     if e == Model.DT:
         train_dt(x_train, x_valid, y_train, y_valid)
     elif e == Model.SVM:
         train_svm(x_train, x_valid, y_train, y_valid)
     elif e == Model.LG:
-        pass
+        train_lg(x_train, x_valid, y_train, y_valid)
     elif e == Model.MLP:
-        pass
-    elif e == SGD_SVM:
+        train_mlp(x_train, x_valid, y_train, y_valid)
+    elif e == Model.SGD_SVM:
         train_sgd_svm(x_train, x_valid, y_train, y_valid)
-        pass
-    elif e == SGD_LG:
+    elif e == Model.SGD_LG:
         train_sgd_lg(x_train, x_valid, y_train, y_valid)
+    elif e == Model.PERCEP:
+        train_lp(x_train, x_valid, y_train, y_valid)
+    elif e == Model.KNN:
         pass
-    elif e == PERCEP:
-        pass
-    elif e == KNN:
-        pass
-    elif e == FOREST: 
-        pass
+    elif e == Model.FOREST:
+        train_rf(x_train, x_valid, y_train, y_valid)
     else:
         train_dt(x_train, x_valid, y_train, y_valid)
         print()
         train_svm(x_train, x_valid, y_train, y_valid)
         print()
+        train_lg(x_train, x_valid, y_train, y_valid)
+        print()
+        train_mlp(x_train, x_valid, y_train, y_valid)
+        print()
         train_sgd_svm(x_train, x_valid, y_train, y_valid)
         print()
         train_sgd_lg(x_train, x_valid, y_train, y_valid)
+        print()
+        train_lp(x_train, x_valid, y_train, y_valid)
+        print()
+        train_rf(x_train, x_valid, y_train, y_valid)
         print()
         # TODO copy all functions from above
     return
