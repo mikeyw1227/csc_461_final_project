c53f9f4483eb53de121753ae23cc6fa8d556d8ce
Chris Paterno <cpaterno@my.uri.edu>
2019-12-08 11:30:08 -0500

    SGD

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import tree, preprocessing
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
@@ -42,6 +43,10 @@ def svm(x, y, params):
     optimized_classifier = GridSearchCV(classifier, params)
     return optimized_classifier.fit(x, y)
 
+def sgd(x, y, params):
+    classifier = SGDClassifier();
+    optimized_classifier = GridSearchCV(classifier, params)
+    return optimized_classifier.fit(x, y)
 
 def multilayer_perceptron(x, y):
     classifier = MLPClassifier()
@@ -60,6 +65,7 @@ def main():
     #              'max_depth': [i for i in range(5, 105, 10)]}
     # dt_classifier = decision_tree(x_train, y_train, dt_params)
     # results(dt_classifier, x_valid, y_valid)
+    '''
     # support vector machine
     svm_params = {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
                   'gamma': ['auto', 'scale'],
@@ -67,9 +73,37 @@ def main():
                   'C': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]}
     svm_classifier = svm(x_train, y_train, svm_params)
     results(svm_classifier, x_valid, y_valid)
+    '''
     # multilayer perceptron
     # mlp_classifier = multilayer_perceptron(x_data, y_data)
-
-
+    # SGD SVM
+    #sgdsvm_params = {
+    #        'loss': ('hinge', 'squared_hinge'),
+    #        'penalty': ('none', 'l1', 'l2', 'elasticnet'), 
+    #        'max_iter': [i for i in range(1000, 10001, 1000)],
+    #        'learning_rate': ('constant', 'optimal', 'invscaling', 'adaptive'),
+    #        'eta0': (0.001,)
+    #}
+    #sgdsvm_params = {
+    #        'loss': ('hinge', 'squared_hinge'),
+    #        'penalty': ('none', 'l1', 'l2', 'elasticnet'), 
+    #        'max_iter': [i for i in range(1000, 10001, 1000)],
+    #}
+    #sgdsvm_params = {
+    #        'loss': ('hinge', 'squared_hinge'),
+    #        'penalty': ('none', 'l1', 'l2', 'elasticnet'), 
+    #        'max_iter': [i for i in range(500, 2001, 250)],
+    #}
+    #sgdsvm_classifier = sgd(x_train, y_train, sgdsvm_params)
+    #results(sgdsvm_classifier, x_valid, y_valid)
+    sgdlg_params = {
+            'loss': ('log',),
+            'penalty': ('none', 'l1', 'l2', 'elasticnet'), 
+            'max_iter': [i for i in range(1000, 10001, 1000)],
+            'learning_rate': ('constant', 'optimal', 'invscaling', 'adaptive'),
+            'eta0': (0.001,)
+    }
+    sgdlg_classifier = sgd(x_train, y_train, sgdlg_params)
+    results(sgdlg_classifier, x_valid, y_valid)
 if __name__ == "__main__":
     main()
